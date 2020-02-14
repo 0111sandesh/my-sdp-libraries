7fc78990ff4c267146b334d50cbd64d52ca7b718
Ubuntu <ubuntu@ip-10-0-1-161.ec2.internal>
2020-02-14 14:45:07 +0000

    updated deploy

--- deploy/deploy.groovy
+++ deploy/deploy.groovy
@@ -1,7 +1,8 @@
 void call(){
 	stage "Deploy", {
 	node{
-        sh "docker run -d -p 80:8080 --name my-sample-app 0.0.0.0:5000/sdp-labs-sample-app:${env.GIT_SHA}"
+        sh "docker rm -f my-sample-app || true"
+	sh "docker run -d -p 80:8080 --name my-sample-app 0.0.0.0:5000/sdp-labs-sample-app:${env.GIT_SHA}"
       }
 
     }
