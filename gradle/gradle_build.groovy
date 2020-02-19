f3dc1498ac9d9decea252223ddf058937bf79dde
0111sandesh <0111sandesh@users.noreply.github.com>
2020-02-19 07:27:54 -0500

    Update gradle_build.groovy

--- gradle/gradle_build.groovy
+++ gradle/gradle_build.groovy
@@ -1,6 +1,7 @@
 void call(){
 	stage "Build gradle app", {
 	node{
+		unstash "workspace"
 		sh "pwd"
 		sh "./gradlew build"
 		sh "md5sum /var/jenkins_home/workspace/sample-myGithub_master/build/libs/gs-spring-boot-0.1.0.jar"
