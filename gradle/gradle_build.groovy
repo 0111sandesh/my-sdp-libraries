3f983beae25dff40aaf03916d0a57810ec3006c4
0111sandesh <0111sandesh@users.noreply.github.com>
2020-02-19 07:44:46 -0500

    Update gradle_build.groovy

--- gradle/gradle_build.groovy
+++ gradle/gradle_build.groovy
@@ -5,6 +5,7 @@ void call(){
 		sh "pwd"
 		sh "./gradlew build"
 		sh "md5sum /var/jenkins_home/workspace/sample-myGithub_master/build/libs/gs-spring-boot-0.1.0.jar"
+		sh "cp /var/jenkins_home/workspace/sample-myGithub_master/build/libs/gs-spring-boot-0.1.0.jar ."
       }
 
     }
